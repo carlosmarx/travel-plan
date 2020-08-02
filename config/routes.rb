22e47d084bdbe9370b2717d9e21060cf5d5bfcef
Carlos Marx <carlosmarx27@gmail.com>
2020-08-02 11:21:10 -0300

    Authentication and authorization wit devise and JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :trips, only: [:index]
+      post 'authenticate', to: 'authentication#authenticate'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+trip_names = [  
+  "Romantic Spots for Honeymoons", 
+  "Top Island Getaways", 
+  "Most Romantic Destinations", 
+  "Top Spots for Skiing" 
+]
+
+trip_names.each do |trip_name|
+  Trip.create(name: trip_name)
+end
